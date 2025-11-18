4494244b892749ecae0008744322f8f4e2d6fbb4
Saurabh Kumar Singh <saurabhs051@gmail.com>
2025-11-18 06:50:30 +0530

    Refactor cycle detection method for clarity and efficiency

--- 01 Data Structure and Algorithms/Graph/02 Cycle Detection/03_cycle_detection_undirected_dfs_visited_tracking.cpp
+++ 01 Data Structure and Algorithms/Graph/02 Cycle Detection/03_cycle_detection_undirected_dfs_visited_tracking.cpp
@@ -1,50 +1,47 @@
-class Solution {
+
     /* 
     *	Cycle detection in Undirected graph : DFS
     *	1. A node should not be already visited (unless it is the immediate parent of current node)
     *	2. Try from each source node, to tackle case of disjoint graph
     *	3. Return early, as soon as cycle found
     */
-    bool isCycle(unordered_map<int, vector<int>>& adj, int src, int parent, vector<bool>& visited)
+class Solution {
+    bool isCyclic(unordered_map<int, vector<int>>& adjList, int node, vector<bool>& visited, int parent)
     {
-        if (!visited[src])
+        if (visited[node])
+            return true;
+            
+        visited[node] = true;
+        for (auto& dest : adjList[node])
         {
-            visited[src] = true;
-            for (auto& neighbour :adj[src])
+            if (dest != parent) // Don't go ahead for parent neighbour
             {
-                if (!visited[neighbour])
-                {
-                    if (isCycle(adj, neighbour, src, visited))
-                        return true;
-                }
-                else
-                {
-					// Visited neighbour which is not parent, i.e. cycle exists
-                    if (neighbour != parent)
-                        return true;
-                }
+                if (isCyclic(adjList, dest, visited, node))
+                    return true;
             }
         }
         return false;
     }
   public:
     bool isCycle(int V, vector<vector<int>>& edges) {
-        if (!edges.size())
+        if (V == 0)
             return false;
-        // Create adjacency list
-        unordered_map<int, vector<int>> adj;
+        
+        unordered_map<int, vector<int>> adjList;
         for (auto& edge : edges)
         {
-            // undirected graph, push both ways
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+            int src = edge[0];
+            int dest = edge[1];
+            adjList[src].push_back(dest);
+            adjList[dest].push_back(src);
         }
         
-        // Find cycle
-        vector<bool> visited(V, 0);
+        vector<bool> visited(V, false);
         for (int src = 0; src < V; ++src)
         {
-            if (isCycle(adj, src, -1, visited))
+            if (visited[src])
+                continue;
+            if (isCyclic(adjList, src, visited, -1))
                 return true;
         }
         return false;
