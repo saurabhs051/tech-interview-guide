832aecf52135da8904a468c709853c69d1b22e61
Saurabh Kumar Singh <saurabhs051@gmail.com>
2025-11-19 13:09:08 +0530

    Refactor comments in Prim's MST implementation
    Removed a comment with a link to an external resource and retained the main description of the problem.

--- 01 Data Structure and Algorithms/Graph/05 Minimum Spanning Tree/02_prim_mst_min_cost_to_connect_houses.cpp
+++ 01 Data Structure and Algorithms/Graph/05 Minimum Spanning Tree/02_prim_mst_min_cost_to_connect_houses.cpp
@@ -1,5 +1,5 @@
 class Solution {
-/* 
+/* Checkout this (better) : https://www.geeksforgeeks.org/dsa/minimum-cost-required-to-connect-all-houses-in-a-city/ [MST : Focus on individual edges only]
 * Given a 2D array houses[][] consisting of n 2D coordinates {x, y} where each coordinate represents the location of each house, the task is to find the minimum cost to connect all the houses of the city.
 * Note: Cost of connecting two houses is the Manhattan Distance between the two points (xi, yi) and (xj, yj) i.e., |xi - xj| + |yi - yj|, where |p| denotes the absolute value of p.
  * 1. Min cost to connect all the houses (MST) : n-1 edges
